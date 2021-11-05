7563a8e60198233e128cd2ed78c40deba61b64dd
Nicole Buendia <70274300+nbuendia@users.noreply.github.com>
2021-11-05 15:44:23 -0400

    repo transfer

--- src/index.js
+++ src/index.js
@@ -1,28 +1,17 @@
-
 import Phaser from "phaser";
+import SpaceScene from "./scenes/spaceScene";
 
 const config = {
   type: Phaser.AUTO,
+  scene: [SpaceScene],
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
-      gravity: { y: 200 }
-    }
+      gravity: { y: 200 },
+    },
   },
-  scene: {
-    preload: preload,
-    create: create
-  }
 };
 
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
